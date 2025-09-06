f9613c2928f6fb58a011b325219e43f10d086f41
Md. Toufiq Imroz Khealid Khan <toufiqimroz161055@gmail.com>
2025-09-06 16:40:55 +0600

    230. Move Zeros
    Problem Link: https://leetcode.com/problems/move-zeroes/

---
+++ 230_Move_Zeros.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int> & nums) {
+        int n = nums.size(); 
+        int cnt = 0;
+
+        int st = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] != 0){
+               nums[st] = nums[i];
+               st++;
+            }
+        }
+        for(int i = st; i<n; i++){
+            nums[i] = 0;
+        }
+    }
+};
