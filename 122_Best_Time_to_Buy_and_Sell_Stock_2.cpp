45f67c5a620cb190497535c3e683bd4c25720524
Md. Toufiq Imroz Khealid Khan <toufiqimroz161055@gmail.com>
2025-09-14 20:05:47 +0600

    122. Best Time to Buy and Sell Stock ||
    Problem Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/

---
+++ 122_Best_Time_to_Buy_and_Sell_Stock_2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans=0;
+        for(int i=1; i<prices.size(); i++){
+            if(prices[i-1]<prices[i]){
+                int temp=prices[i-1];
+                int j=i+1;
+                while((j < prices.size()) && (prices[j-1] < prices[j])){
+                    j++; 
+                }
+                ans += prices[j-1]-temp;
+                i=j-1;
+            }
+        }
+        return ans;
+    }
+};
